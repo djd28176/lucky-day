efb278bb59bea5042bf7038d2338b1dfed9bc109
jindongdu <jindong.du@tabnerglobal.com>
2023-11-29 11:27:25 -0500

    Initial Commit

---
+++ slot-machine/slot_machineApp.swift
@@ -0,0 +1,17 @@
+//
+//  slot_machineApp.swift
+//  slot-machine
+//
+//  Created by Jindong Du on 11/29/23.
+//
+
+import SwiftUI
+
+@main
+struct slot_machineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
