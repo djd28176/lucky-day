2c81da1ede15a3073d3c2c248fbf61f5653fb3f0
jindongdu <djd28176@hotmail.com>
2023-11-29 16:24:11 -0500

    lucky day

--- slot-machine/SwiftUIView.swift
+++ slot-machine/SwiftUIView.swift
@@ -7,12 +7,30 @@
 
 import SwiftUI
 
-struct SwiftUIView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct Hexagonal: Shape {
+    func path(in rect: CGRect)->Path{
+        return Path{
+            path in
+            let p1 = CGPoint(x: 0, y: 20)
+            let p2 = CGPoint(x: 0, y: rect.height - 20)
+            let p3 = CGPoint(x: rect.width/2, y: rect.height)
+            let p4 = CGPoint(x: rect.width, y: rect.height-20)
+            let p5 = CGPoint(x: rect.width, y: 20)
+            let p6 = CGPoint(x: rect.width/2, y: 0)
+            
+            path .move(to: p6)
+            path.addArc(tangent1End: p1, tangent2End: p2, radius: 15)
+            path.addArc(tangent1End: p2, tangent2End: p3, radius: 15)
+            path.addArc(tangent1End: p3, tangent2End: p4, radius: 15)
+            path.addArc(tangent1End: p4, tangent2End: p5, radius: 15)
+            path.addArc(tangent1End: p5, tangent2End: p6, radius: 15)
+            path.addArc(tangent1End: p6, tangent2End: p1, radius: 15)
+        }
     }
+    
 }
 
 #Preview {
-    SwiftUIView()
+    Hexagonal()
+        .frame(width:100,height: 120,alignment: .center)
 }
