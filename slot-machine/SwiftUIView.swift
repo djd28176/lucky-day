a82530d3b279685377599cb2c25b0a53435869b3
jindongdu <djd28176@hotmail.com>
2023-11-29 16:19:15 -0500

    lucky day slot machince

---
+++ slot-machine/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  slot-machine
+//
+//  Created by Jindong Du on 11/29/23.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
